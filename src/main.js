df05f562b5a26e35c0024b6a8e95d0e53e175b80
M. Khaidar <62201903+haidar038@users.noreply.github.com>
2023-05-21 18:24:08 +0900

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -20,27 +20,110 @@ window.addEventListener("scroll", () => {
     });
 });
 
-//This function will be called when user click changing language
-function translate(lng, tagAttr) {
-    var translate = new Translate();
-    translate.init(tagAttr, lng);
-    translate.process();
-    if (lng == "en") {
-        $("#enTranslator").css("color", "#f4623a");
-        $("#idTranslatoe").css("color", "#212529");
+document.addEventListener("DOMContentLoaded", function () {
+    const carousel = document.querySelector(".carousel");
+    const carouselInner = document.querySelector(".carousel-inner");
+    const prevBtn = document.getElementById("prev-btn");
+    const nextBtn = document.getElementById("next-btn");
+
+    const mobileQuery = window.matchMedia("(max-width: 767.9px)");
+    const tabletQuery = window.matchMedia("(max-width: 1023px)");
+
+    if (mobileQuery.matches) {
+        const cardsPerSlide = 1;
+
+        // Lebar satu card termasuk margin
+        const cardWidth = carouselInner.querySelector(".card").offsetWidth + 16;
+
+        // Jumlah total slide
+        const totalSlides = Math.ceil(carouselInner.children.length / cardsPerSlide);
+
+        let currentSlide = 0;
+
+        // Fungsi untuk menggeser slide ke kiri
+        const slideToPrev = function () {
+            if (currentSlide > 0) {
+                currentSlide--;
+                carousel.scrollLeft = currentSlide * (cardWidth * cardsPerSlide);
+            }
+        };
+
+        // Fungsi untuk menggeser slide ke kanan
+        const slideToNext = function () {
+            if (currentSlide < totalSlides - 1) {
+                currentSlide++;
+                carousel.scrollLeft = currentSlide * (cardWidth * cardsPerSlide);
+            }
+        };
+
+        // Tambahkan event listener pada tombol prev
+        prevBtn.addEventListener("click", slideToPrev);
+
+        // Tambahkan event listener pada tombol next
+        nextBtn.addEventListener("click", slideToNext);
+    } else if (tabletQuery.matches) {
+        const cardsPerSlide = 2;
+
+        // Lebar satu card termasuk margin
+        const cardWidth = carouselInner.querySelector(".card").offsetWidth + 16;
+
+        // Jumlah total slide
+        const totalSlides = Math.ceil(carouselInner.children.length / cardsPerSlide);
+
+        let currentSlide = 0;
+
+        // Fungsi untuk menggeser slide ke kiri
+        const slideToPrev = function () {
+            if (currentSlide > 0) {
+                currentSlide--;
+                carousel.scrollLeft = currentSlide * (cardWidth * cardsPerSlide);
+            }
+        };
+
+        // Fungsi untuk menggeser slide ke kanan
+        const slideToNext = function () {
+            if (currentSlide < totalSlides - 1) {
+                currentSlide++;
+                carousel.scrollLeft = currentSlide * (cardWidth * cardsPerSlide);
+            }
+        };
+
+        // Tambahkan event listener pada tombol prev
+        prevBtn.addEventListener("click", slideToPrev);
+
+        // Tambahkan event listener pada tombol next
+        nextBtn.addEventListener("click", slideToNext);
+    } else {
+        const cardsPerSlide = 3;
+
+        // Lebar satu card termasuk margin
+        const cardWidth = carouselInner.querySelector(".card").offsetWidth + 16;
+
+        // Jumlah total slide
+        const totalSlides = Math.ceil(carouselInner.children.length / cardsPerSlide);
+
+        let currentSlide = 0;
+
+        // Fungsi untuk menggeser slide ke kiri
+        const slideToPrev = function () {
+            if (currentSlide > 0) {
+                currentSlide--;
+                carousel.scrollLeft = currentSlide * (cardWidth * cardsPerSlide);
+            }
+        };
+
+        // Fungsi untuk menggeser slide ke kanan
+        const slideToNext = function () {
+            if (currentSlide < totalSlides - 1) {
+                currentSlide++;
+                carousel.scrollLeft = currentSlide * (cardWidth * cardsPerSlide);
+            }
+        };
+
+        // Tambahkan event listener pada tombol prev
+        prevBtn.addEventListener("click", slideToPrev);
+
+        // Tambahkan event listener pada tombol next
+        nextBtn.addEventListener("click", slideToNext);
     }
-    if (lng == "id") {
-        $("#idTranslatoe").css("color", "#f4623a");
-        $("#enTranslator").css("color", "#212529");
-    }
-}
-$(document).ready(function () {
-    //This is id of HTML element (English) with attribute lng-tag
-    $("#enTranslator").click(function () {
-        translate("en", "lng-tag");
-    });
-    //This is id of HTML element (idmer) with attribute lng-tag
-    $("#idTranslator").click(function () {
-        translate("id", "lng-tag");
-    });
 });
